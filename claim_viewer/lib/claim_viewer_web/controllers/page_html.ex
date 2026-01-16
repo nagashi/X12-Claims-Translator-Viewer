a83d8cddc34f223e421a48a1c427f7ac1a66e839
Irini1996 <iringega@gmail.com>
2026-01-17 00:24:49 +0100

    Update

--- claim_viewer/lib/claim_viewer_web/controllers/page_html.ex
+++ claim_viewer/lib/claim_viewer_web/controllers/page_html.ex
@@ -7,4 +7,34 @@ defmodule ClaimViewerWeb.PageHTML do
   use ClaimViewerWeb, :html
 
   embed_templates "page_html/*"
+
+def human_label(key) do
+  %{
+    "controlNumber" => "Control Number",
+    "referenceId" => "Reference ID",
+    "payerId" => "Payer ID",
+    "totalCharge" => "Total Charge",
+    "serviceDate" => "Service Date",
+    "firstName" => "First Name",
+    "lastName" => "Last Name",
+    "groupNumber" => "Group Number",
+    "planType" => "Plan Type",
+    "taxId" => "Tax ID",
+    "clearinghouseClaimNumber" => "Claim Number",
+    "dob" => "Date of Birth",
+    "npi" => "NPI",
+    "onsetDate" => "Onset Date",
+    "placeOfService" => "Place of Service",
+    "serviceType" => "Service Type",
+    "unitQualifier" => "Unit Qualifier",
+    "lineNumber" => "Line #",
+    "procedureCode" => "Procedure Code"
+  }[key] ||
+    key
+    |> Macro.underscore()
+    |> String.replace("_"," ")
+    |> String.capitalize()
+end
+
+
 end
